7592e74bb54729a172afb842106234fd8bd829e2
kimlevcenko <levchenko.kim@icloud.com>
2023-05-25 14:13:06 +0500

    выполнение 13 домашнего задания

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String lastName;
@@ -13,5 +15,22 @@ public class Author {
 
     public String getLastName() {
         return this.lastName;
+    }
+
+    public String toString() {
+        return "Автор: " + this.name + " " + this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,24 @@ public class Main {
         Book book2 = new Book("Our tragic universe", 2010, author2);
         System.out.println("Book 1 = " + book1.getAuthor().getName() + " " + book1.getAuthor().getLastName() + " " + book1.getName() + " " + book1.getDatePublication());
         System.out.println("Book 2 = " + book2.getAuthor().getName() + " " + book2.getAuthor().getLastName() + " " + book2.getName() + " " + book2.getDatePublication());
-
+        System.out.println();
         book1.setDatePublication(2020);
         System.out.println("book1.getDatePublication() = " + book1.getDatePublication());
+        System.out.println();
+        System.out.println(author1);
+        System.out.println(author2);
+        System.out.println();
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println();
+        if (book1.equals(book2)) {
+            System.out.println("Проверьте наименование, такая книга уже существует");
+        } else
+            System.out.println("Новая книга добавлена");
+        if (author1.equals(author2)) {
+            System.out.println("Проверьте данные, автор с такой фамилией уже существует");
+        } else
+            System.out.println("Новый автор добавлен");
     }
+
 }
